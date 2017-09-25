a4bcb91f75326009f5d50b7e72c2916ad8680b55
Molinetix <molinero93@gmail.com>
2017-09-25 10:41:51 +0200

    Actualizaciones pendientes

--- TFG/src/controlador/ControladorRegistro.java
+++ TFG/src/controlador/ControladorRegistro.java
@@ -7,6 +7,8 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.swing.JOptionPane;
 import modelo.TratamientoBD;
 import vista.JFRegistro;
@@ -15,25 +17,38 @@ import vista.JFRegistro;
  *
  * @author Sergio
  */
-public class ControladorRegistro implements ActionListener{
-    
+public class ControladorRegistro implements ActionListener {
+
     TratamientoBD bd = new TratamientoBD();
     JFRegistro registro = new JFRegistro();
 
-    public ControladorRegistro(TratamientoBD bd, JFRegistro registro){
+    public ControladorRegistro(TratamientoBD bd, JFRegistro registro) {
         this.bd = bd;
         this.registro = registro;
-        
+
         registro.btnRegistro.addActionListener(this);
         registro.btnVolver.addActionListener(this);
     }
-    
-    
+
+    public boolean validarCorreo(String mail) {
+        boolean validado = false;
+        Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+
+        Matcher mather = pattern.matcher(mail);
+
+        if (mather.find() == true) {
+            validado = true;
+        }
+
+        return validado;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()== registro.btnRegistro){
+        if (e.getSource() == registro.btnRegistro) {
             JOptionPane.showMessageDialog(null, "Se registrará el usuario...");
-            
+
             String nombre = registro.txtNombre.getText();
             String apellido = registro.txtApellidos.getText();
             String fecha = String.valueOf(registro.jXDatePicker.getDate());
@@ -43,12 +58,10 @@ public class ControladorRegistro implements ActionListener{
             String password = registro.txtPass.getText();
             String password2 = registro.txtPass2.getText();
         }
-        
-        if(e.getSource() == registro.btnVolver){
+
+        if (e.getSource() == registro.btnVolver) {
             registro.dispose();
         }
     }
-    
-    
-    
+
 }
